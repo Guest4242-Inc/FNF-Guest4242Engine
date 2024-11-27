f35c4ae90e685e5f8aa3da1a6a0d8279b0c1bf84
imguest24897-alt <161818573+imguest24897-alt@users.noreply.github.com>
2024-11-27 12:38:01 +0200

    edited a bit the push script

--- tools/push.sh
+++ tools/push.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
+# Made with Vim on Arch Linux ^^
 echo "Pushing origin..."
 git push origin main # TODO: Check which branch contains the game source
 if [ $? -ne 0 ]; then
-	echo "Error: git push failed!"
+	echo "Error: git push failed!" # TODO: Show more details
 	exit 1
 fi
 
