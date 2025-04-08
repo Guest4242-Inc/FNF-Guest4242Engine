17baf9d88eb2b544e2f4c2687ce83c7d10d34eb9
imguest24897-alt <161818573+imguest24897-alt@users.noreply.github.com>
2025-04-08 10:16:47 +0300

    update stuff

--- source/funkin/ui/title/OutdatedSubState.hx
+++ source/funkin/ui/title/OutdatedSubState.hx
@@ -19,22 +19,24 @@ class OutdatedSubState extends MusicBeatState
     add(bg);
     var ver = "v" + Application.current.meta.get('version');
     var txt:FlxText = new FlxText(0, 0, FlxG.width,
-      "HEY! You're running an outdated version of the game!\nCurrent version is "
+      "Ay bro, you are using an outdated version of the Guest4242 Engine! "
+      + "You are using version "
       + ver
       + " while the most recent version is "
       + NGio.GAME_VER
-      + "! Press Space to go to itch.io, or ESCAPE to ignore this!!",
+      + "! Press Space to go to our GitHub, or ESCAPE to ignore this!!",
       32);
     txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
     txt.screenCenter();
     add(txt);
+    openfl.Lib.application.window.title = "Friday Night Funkin': Guest4242 Engine (OUTDATED!!)";
   }
 
   override function update(elapsed:Float)
   {
     if (controls.ACCEPT)
     {
-      FlxG.openURL("https://ninja-muffin24.itch.io/funkin");
+      FlxG.openURL("https://github.com/Guest4242-Inc/FNF-Guest4242Engine/releases");
     }
     if (controls.BACK)
     {
